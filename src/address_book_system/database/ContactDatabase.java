0cd85e310122f253c37c59795f25aa81e43ef94e
Arun Gowda <arungowdaarungowda36@gmail.com>
2024-11-14 12:17:59 +0530

    adding database file of address book system inyo git from databse branch. it contain data of contacts.

---
+++ src/address_book_system/database/ContactDatabase.java
@@ -0,0 +1,15 @@
+package address_book_system.database;
+
+import address_book_system.entity.Person;
+
+import java.util.HashMap;
+
+public class ContactDatabase {
+
+    public HashMap<String, Person> contactDatabase(){
+        return new HashMap<>();
+    }
+
+
+
+}
